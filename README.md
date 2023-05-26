b7ec615a8142ff17e9df69ee83fc2b0da59834fa
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-05-26 17:41:47 +0600

    changes text

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ generate images with words
 1. Listen to clicks
 2. If there is any text, generate a result.
 3. If there is no text, do notghing
+
+Node.js
+
+Alternatively, if you demand a more responsive setup and already use nodejs...
+
+Install http-server by typing npm install -g http-server
+Change into your working directory, where yoursome.html lives
+Start your http server by issuing http-server -c-1
+This spins up a Node.js httpd which serves the files in your directory as static files accessible from http://localhost:8080
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const setupInputContainer = document.getElementById('setup-input-container')
 const businessBossText = document.getElementById('business-boss-text')
 
 /* API vars*/
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 const apiKey = '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'
 const url = 'https://api.openai.com/v1/completions'
 
@@ -44,19 +44,5 @@ TODO:
     }),
   })
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => (businessBossText.innerText = data.choices[0].text))
 }
-
-// fetch(url, {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     Authorization: `Bearer ${apiKey}`,
-//   },
-//   body: JSON.stringify({
-//     model: 'text-davinci-003',
-//     prompt: 'What is the best business idea?',
-//   }),
-// })
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
