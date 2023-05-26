caf07df730e7b27f15d432644c03d5ec30c57bed
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-05-26 20:21:40 +0600

    new

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const process = {
   env: {
-    OPENAI_API_KEY: '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',
+    OPENAI_API_KEY: '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',
   },
 }
