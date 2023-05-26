88878e39719603c76e6debd9867edafc1eb6749f
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-05-26 17:27:15 +0600

    add

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+// if (typeof document !== 'undefined') {
+//   console.log('Running in browser')
+// } else {
+//   console.log('Running in browserwbejdkns')
+// }
+
 const setupTextarea = document.getElementById('setup-textarea')
 const setupInputContainer = document.getElementById('setup-input-container')
 const businessBossText = document.getElementById('business-boss-text')
 
+/* API vars*/
+const fetch = require('node-fetch')
+const apiKey = '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'
+const url = 'https://api.openai.com/v1/completions'
+
 document.getElementById('send-btn').addEventListener('click', () => {
   //   if (setupTextarea.value) {
-  setupInputContainer.innerHTML = `<img src="images/loading_TODO.svg" class="loading" id="loading">`
-  businessBossText.innerText = `wait for AI result...`
+  //   setupInputContainer.innerHTML = `<img src="images/loading_TODO.svg" class="loading" id="loading">`
+  //   businessBossText.innerText = data
   //   }
 
   fetchAIreply()
@@ -14,30 +25,38 @@ document.getElementById('send-btn').addEventListener('click', () => {
 function fetchAIreply() {
   /*
 TODO:
-  1. Make a fetch request to OpenAi API. 
+  1. Make a fetch request to OpenAi API.
   2. The prompt should request an enthusiastic response
-     in no more than 5 words. 
-  3. For now you can just log out the completion to check 
-     it's working. 
+     in no more than 5 words.
+  3. For now you can just log out the completion to check
+     it's working.
 */
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: 'Give me and enthusiastic response in no more than 5 words.',
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
 }
 
-const fetch = require('node-fetch')
-
-const apiKey = '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'
-
-const url = 'https://api.openai.com/v1/completions'
-
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${apiKey}`,
-  },
-  body: JSON.stringify({
-    model: 'text-davinci-003',
-    prompt: 'What is the best business idea?',
-  }),
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data))
+// fetch(url, {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Authorization: `Bearer ${apiKey}`,
+//   },
+//   body: JSON.stringify({
+//     model: 'text-davinci-003',
+//     prompt: 'What is the best business idea?',
+//   }),
+// })
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
