897474d2ba36713878d56a79efe65d31ce051989
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-05-26 17:04:19 +0600

    todo fetch generic reply

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-// const setupTextarea = document.getElementById('setup-textarea')
-// const setupInputContainer = document.getElementById('setup-input-container')
-// const businessBossText = document.getElementById('business-boss-text')
+const setupTextarea = document.getElementById('setup-textarea')
+const setupInputContainer = document.getElementById('setup-input-container')
+const businessBossText = document.getElementById('business-boss-text')
 
-// document.getElementById('send-btn').addEventListener('click', () => {
-//   if (setupTextarea.value) {
-//     setupInputContainer.innerHTML = `<img src="images/loading_TODO.svg" class="loading" id="loading">`
-//     businessBossText.innerText = `wait for AI result...`
-//   }
-// })
+document.getElementById('send-btn').addEventListener('click', () => {
+  //   if (setupTextarea.value) {
+  setupInputContainer.innerHTML = `<img src="images/loading_TODO.svg" class="loading" id="loading">`
+  businessBossText.innerText = `wait for AI result...`
+  //   }
+
+  fetchAIreply()
+})
+
+function fetchAIreply() {
+  /*
+TODO:
+  1. Make a fetch request to OpenAi API. 
+  2. The prompt should request an enthusiastic response
+     in no more than 5 words. 
+  3. For now you can just log out the completion to check 
+     it's working. 
+*/
+}
 
 const fetch = require('node-fetch')
 
