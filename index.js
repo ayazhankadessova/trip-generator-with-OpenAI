15978e2ad005b3cb675a698a7777d9792ce0a1da
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-05-27 11:56:59 +0600

    fix: control length

--- index.js
+++ index.js
@@ -16,28 +16,32 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 document.getElementById('send-btn').addEventListener('click', () => {
-  //   if (setupTextarea.value) {
-  //   setupInputContainer.innerHTML = `<img src="images/loading_TODO.svg" class="loading" id="loading">`
-  //   businessBossText.innerText = data
-  //   }
-
-  fetchAIreply()
+  if (setupTextarea.value) {
+    const userInput = setupTextarea.value
+    setupInputContainer.innerHTML = `<img src="images/bookSuggest.png" class="loading" id="loading">`
+    businessBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+    fetchAIreply(userInput)
+  }
 })
 
 /*
 TODO:
-  1. Make a fetch request to OpenAi API.
-  2. The prompt should request an enthusiastic response
-     in no more than 5 words.
-  3. For now you can just log out the completion to check
-     it's working.
+    1. Make a fetch request to OpenAi API.
+    2. The prompt should request an enthusiastic response
+        in no more than 5 words.
+    3. For now you can just log out the completion to check
+        it's working.
+
+    ----------------
+    1. Refactor this prompt so the AI gives an enthusiastic, personalised 
+        response to the users input and says it needs a few moments to think.
 */
-async function fetchAIreply() {
+async function fetchAIreply(userInput) {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: 'Suggest me a book about feminism, non-fiction, not old.\n',
+    prompt: `Give me an enthusiastic reply about ${userInput}`,
     temperature: 1,
-    max_tokens: 25,
+    max_tokens: 60,
     top_p: 0.7,
     frequency_penalty: 0,
     presence_penalty: 0,
